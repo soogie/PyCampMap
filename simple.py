86ebd4099d048f3db13d49526264a6dbcddfbdd1
Takeshi Sugiyama <kawacub@gmail.com>
2017-12-25 15:11:02 +0000

    add IP setting

--- simple.py
+++ simple.py
@@ -82,4 +82,4 @@ def main():
 
 if __name__ == '__main__':
     #main()
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host=os.environ.get("IP", '0.0.0.0'), port=int(os.environ.get("PORT", 5000)), debug=False)
